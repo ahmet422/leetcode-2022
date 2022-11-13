9c7eb7ae558f87c307aeb4824f7d9720ee525551
ahmet422 <ahmet.tajik@icloud.com>
2022-11-13 17:31:54 -0600

    Initial commit

---
+++ src/easy/LongestCommonPrefix.java
@@ -0,0 +1,12 @@
+package easy;
+// link: https://leetcode.com/problems/longest-common-prefix/description/
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+
+        return "";
+    }
+}
